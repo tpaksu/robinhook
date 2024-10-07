57f60ed41769b0c3137cc302ccd06f83bf2a52a7
Taha Paksu <tpaksu@gmail.com>
2024-10-07 07:13:27 +0000

    Add domain specific routes

--- server.php
+++ server.php
@@ -4,9 +4,4 @@ use Tpaksu\Robinhook\Websocket;
 require 'vendor/autoload.php';
 
 $server = new Websocket();
-
-try {
-    $server->startServer(5501);
-}catch(RuntimeException $ex){
-    echo $ex->getMessage();
-}
+$server->startServer(5501);
--- src/Robinhook.php
+++ src/Robinhook.php
@@ -22,19 +22,30 @@ class Robinhook
 
     public function initRoutes()
     {
-        // map a route
-        $this->router->map('POST', '/webhook', function (
-            ServerRequestInterface $request
-        ): array {
-            $wsClient = new Client('ws://0.0.0.0:5501');
-            $wsClient->send(json_encode(\Laminas\Diactoros\Request\ArraySerializer::toArray($request)));
-            $wsClient->close();
-            return ["[accepted]"];
-        });
-        $this->router->map("GET", "/", function () {
+        foreach([
+            "/adyen/account/webhook",
+            "/adyen/dispute/webhook",
+            "/adyen/payment_processing/webhook",
+            "/adyen/transfer/webhook"
+        ] as $target){
+            // map a route
+            $this->router->map('POST', $target, function (ServerRequestInterface $request) {
+                return $this->redirectRequest($request);    
+            });
+        }
+        
+        $this->router->map("GET", "/",  function(){
             return [
                 "welcome to tpaksu's personal webhook redirector (robinhook)"
             ];
         });
+    }
+
+    public function redirectRequest($request) {
+        $wsClient = new Client('ws://0.0.0.0:5501');
+        $wsClient->send(json_encode(\Laminas\Diactoros\Request\ArraySerializer::toArray($request)));
+        $wsClient->close();
+        
+        return new \Laminas\Diactoros\Response\TextResponse( "[accepted]", 200, ['Content-Type' => ['application/json']] );
     }
 }
